2584d27b705b25134a4633f8f2c8a76248b9d934
stylianosphedonos <stylianos.phedonos@gmail.com>
2026-03-31 19:59:42 +0300

    Deployment issue

--- src/index.js
+++ src/index.js
@@ -205,7 +205,8 @@ app.get('/health', (req, res) => {
 app.use(express.static(path.join(process.cwd(), 'public')));
 
 // SPA fallback - serve index.html for non-API routes that don't match static files
-app.get('*', (req, res, next) => {
+app.use((req, res, next) => {
+  if (req.method !== 'GET') return next();
   if (req.path.startsWith('/api/')) return next();
   res.sendFile(path.join(process.cwd(), 'public', 'index.html'), (err) => {
     if (err) next();
